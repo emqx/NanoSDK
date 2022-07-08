6a225874b68d4efad8e8a330f8794340f37408b8
wanghaemq <wanghamax@gmail.com>
2022-07-08 16:22:20 +0800

    * FIX [mqttv5] Using name interface name of setting property.

--- demo/mqtt/mqttv5_client.c
+++ demo/mqtt/mqttv5_client.c
@@ -123,8 +123,8 @@ client_connect(nng_socket *sock, const char *url, bool verbose)
 	nng_mqtt_msg_set_connect_will_topic(connmsg, "will_topic");
 	nng_mqtt_msg_set_connect_clean_session(connmsg, true);
 
-	property * p = property_alloc();
-	nni_msg_set_property(connmsg, p);
+	property * p = mqtt_property_alloc();
+	nng_mqtt_msg_set_property(connmsg, p);
 
 	nng_mqtt_set_connect_cb(*sock, connect_cb, &sock);
 	nng_mqtt_set_disconnect_cb(*sock, disconnect_cb, connmsg);
