d6dcec9feb1dcb66844c8f5e7923f9c012f894ba
Jaylin <jaylin@emqx.io>
2023-04-15 15:43:10 +0800

    * FIX https://github.com/emqx/nanomq/issues/1153

--- src/core/message.c
+++ src/core/message.c
@@ -697,6 +697,10 @@ nni_msg_get_pub_pid(nni_msg *m)
 
 	pos = nni_msg_body(m);
 	NNI_GET16(pos, len);
-	NNI_GET16(pos + len + 2, pid);
-	return pid;
+	if (len > nni_msg_remaining_len(m) - 2)
+		return 0;
+	else {
+		NNI_GET16(pos + len + 2, pid);
+		return pid;
+	}
 }
