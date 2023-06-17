2fa2dea9375dcb52568dd71f2876ebed86b3e77c
jaylin <jaylin@emqx.io>
2023-06-17 17:20:46 +0800

    * MDF [mqtt_client] init packetid in sock_init

--- src/mqtt/protocol/mqtt/mqttv5_client.c
+++ src/mqtt/protocol/mqtt/mqttv5_client.c
@@ -106,6 +106,7 @@ mqtt_sock_init(void *arg, nni_sock *sock)
 
 	nni_atomic_init_bool(&s->closed);
 	nni_atomic_set_bool(&s->closed, false);
+	nni_atomic_set(&s->next_packet_id, 1);
 
 	// this is "semi random" start for request IDs.
 	s->retry      = NNI_SECOND * 60;
@@ -262,7 +263,6 @@ mqtt_pipe_init(void *arg, nni_pipe *pipe, void *s)
 	p->pipe      = pipe;
 	p->mqtt_sock = s;
 	p->rid       = 1;
-	nni_atomic_set(&p->mqtt_sock->next_packet_id, 1);
 	nni_aio_init(&p->send_aio, mqtt_send_cb, p);
 	nni_aio_init(&p->recv_aio, mqtt_recv_cb, p);
 	nni_aio_init(&p->time_aio, mqtt_timer_cb, p);
