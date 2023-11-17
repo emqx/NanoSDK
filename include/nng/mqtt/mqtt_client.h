12f68c8baba28599f312a3f4ff5f71898ef0403a
eeff <33566201+eeff@users.noreply.github.com>
2023-11-17 15:07:50 +0800

    chore: enlarge mqtt client recv/send queue size to 2048

--- include/nng/mqtt/mqtt_client.h
+++ include/nng/mqtt/mqtt_client.h
@@ -146,8 +146,8 @@ extern "C" {
 // NNG_MAX_RECV_LMQ and NNG_MAX_SEND_LMQ define the length of waiting queue
 // they are the length of nni_lmq, please be ware it affects the memory usage
 // significantly while having heavy throughput
-#define NNG_MAX_RECV_LMQ 1024
-#define NNG_MAX_SEND_LMQ 1024
+#define NNG_MAX_RECV_LMQ 2048
+#define NNG_MAX_SEND_LMQ 2048
 #define NNG_TRAN_MAX_LMQ_SIZE 128
 
 // NNG_TLS_xxx options can be set on the client as well.
