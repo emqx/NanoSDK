ef6bd9192d4761024b6cce7a41eba66dae3c1407
wayne <yukang.wei@emqx.io>
2023-12-19 19:00:43 +0800

    * MDF [transport] reduce backoff starting time to 0-1s
    Signed-off-by: wayne <yukang.wei@emqx.io>

--- src/mqtt/transport/tcp/mqtt_tcp.c
+++ src/mqtt/transport/tcp/mqtt_tcp.c
@@ -1479,7 +1479,7 @@ mqtt_tcptran_ep_connect(void *arg, nni_aio *aio)
 	if (ep->backoff != 0) {
 		ep->backoff = ep->backoff * 2;
 		ep->backoff = ep->backoff > ep->backoff_max
-		    ? nni_random() % 2000
+		    ? nni_random() % 1000
 		    : ep->backoff;
 		nni_msleep(ep->backoff);
 	} else {
