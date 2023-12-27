8934765c7e6007306d5ad2bfd1f36fbd9b0698f5
wayne <yukang.wei@emqx.io>
2023-12-27 18:32:51 +0800

    * fix
    Signed-off-by: wayne <yukang.wei@emqx.io>

--- src/mqtt/transport/tls/mqtt_tls.c
+++ src/mqtt/transport/tls/mqtt_tls.c
@@ -1488,7 +1488,6 @@ mqtts_tcptran_ep_connect(void *arg, nni_aio *aio)
 		ep->backoff = ep->backoff > ep->backoff_max
 		    ? (nni_duration) (nni_random() % 1000)
 		    : ep->backoff;
-		log_debug("reconnect in %ld", ep->backoff);
 		nni_msleep(ep->backoff);
 	} else {
 		ep->backoff = nni_random()%2000;
