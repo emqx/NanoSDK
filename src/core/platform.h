bff2015ab65483dd14204e28bcb86fa50946dcc3
wayne <yukang.wei@emqx.io>
2023-12-29 13:47:33 +0800

    * FIX [log] fix missing fn for log file
    Signed-off-by: wayne <yukang.wei@emqx.io>

--- src/core/platform.h
+++ src/core/platform.h
@@ -547,8 +547,14 @@ extern char *nni_plat_join_dir(const char *, const char *);
 // string, and may not be altered.
 extern const char *nni_plat_file_basename(const char *);
 
+// nni_plat_file_exists return if file is exists.
+extern bool nni_plat_file_exists(const char *);
+
 // nni_plat_getcwd get current directory where program is running.
 extern char *nni_plat_getcwd(char *, size_t );
+
+// nni_plat_file_size get file size.
+extern int nni_plat_file_size(const char *, size_t *);
 
 // NanoSDK
 // Get standard/UNIX timestamp
